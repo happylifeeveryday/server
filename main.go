9c0547c8804d9cc0f9b8d83b9630c8aa42dcaddf
yihan <yihanshang0@gmail.com>
2025-03-21 14:13:00 +1100

    finish golang 1.5.

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
+import "net/http"
 
 func main() {
 	serveMux := http.NewServeMux()
-	server := http.Server{}
-	server.Handler = serveMux
-	server.Addr = ":8080"
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatalf("error listen and serve")
+	serveMux.Handle("/", http.FileServer(http.Dir("./")))
+	// ServeMux implements Handler interface
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: serveMux,
 	}
+	server.ListenAndServe()
 
 }
