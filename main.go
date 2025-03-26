975650c3b24d95dbab69205f6b98e4a952c0e58b
yihan <yihanshang0@gmail.com>
2025-03-26 17:40:55 +1100

    Finish 11

--- main.go
+++ main.go
@@ -4,8 +4,16 @@ import "net/http"
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir("./")))
 	// ServeMux implements Handler interface
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+	serveMux.Handle("/app/assets/", http.StripPrefix("/app/assets/", http.FileServer(http.Dir("./assets/"))))
+
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
